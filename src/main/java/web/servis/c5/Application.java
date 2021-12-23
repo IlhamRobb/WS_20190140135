8e245800041286a068da2c1354a89f02f09a7736
Ilham R <ilham.r.ft19@mail.umy.ac.id>
2021-12-23 14:36:37 +0700

    first commit

---
+++ src/main/java/web/servis/c5/Application.java
@@ -0,0 +1,13 @@
+package web.servis.c5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
